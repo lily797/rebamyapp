74e64a92c497fdc05cc7792756c8717bc56c3d8f
wanghan <813828498@qq.com>
2022-05-06 06:03:45 +0800

    配置代理

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Child from './chiled'
+import './App.css'
+import { useEffect } from 'react';
+import axios from 'axios';
+function App() {
+  useEffect(() => {
+    axios.get('/api/mmdb/movie/v3/list/hot.json?ct=%E6%88%90%E9%83%BD&ci=59&channelId=4').then(res => {
+      console.log('res',res)
+    })
+    
+  },[])
+  return <div>
+    <ul>
+      <li>1111</li>
+      <li>2222</li>
+      </ul>
+    <Child/>
+  </div>
+}
+
+export default App;
---
+++ src/chiled.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './child.module.scss'
+export default function chiled() {
+  return (
+    <div>chiled
+      <li className={style.item} >3333</li>
+    </div>
+  )
+}
