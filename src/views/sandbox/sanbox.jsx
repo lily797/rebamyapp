9d50db582600f3330c51dad3c81aeb9df3df32b8
wanghan <813828498@qq.com>
2022-05-06 06:53:15 +0800

    新建路由匹配路由

--- src/views/sandbox/sanbox.jsx
+++ src/views/sandbox/sanbox.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {Switch, Route} from 'react-router-dom'
 
+import TopHeader from '../../components/sanbox/TopHeader'
+import SideMenu from '../../components/sanbox/SideMenu'
+import Home from './home/home'
+import UserList from './user-manage'
+import RoleList from './right-manage/role'
+import RightList from './right-manage/right'
 export default function sanbox() {
   return (
-    <div>sanbox</div>
+    <div>
+      <TopHeader />
+      <SideMenu />
+      <Switch>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/user-manage/list" component={UserList}></Route>
+        <Route path="/right-manage/role/list" component={RoleList}></Route>
+        <Route path="/right-manage/right/list" component={RightList}></Route>
+      </Switch>
+    </div>
   )
 }
