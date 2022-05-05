cb646e79426c9ba7b831d8de452eefb4eadff20a
wanghan <813828498@qq.com>
2022-05-06 06:31:56 +0800

    路由守卫-路由重定向

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import Login from "../views/login/login";
 import Sanbox from "../views/sandbox/sanbox";
 export default function IndexRouter() {
@@ -7,7 +7,17 @@ export default function IndexRouter() {
         <HashRouter>
             <Switch>
                 <Route path="/login" component={Login}></Route>
-                <Route path="/" component={Sanbox}></Route>
+                {/* <Route path="/" component={Sanbox}></Route> */}
+                <Route
+                    path="/"
+                    render={() => {
+                       return localStorage.getItem("token") ? (
+                            <Sanbox></Sanbox>
+                        ) : (
+                            <Redirect to="/login"></Redirect>
+                        );
+                    }}
+                ></Route>
             </Switch>
         </HashRouter>
     );
